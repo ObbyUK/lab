85d78a10b4c6ed5c9590f4ae756427da08575e05
Cikito <ciki@obby.co.uk>
2018-12-05 17:07:49 +0200

    reverted back to charge

--- languages/src/rootEpic.js
+++ languages/src/rootEpic.js
@@ -3,6 +3,7 @@ import { combineEpics } from 'redux-observable';
 import sendEmailOnSubmitEmailEpic from './epics/sendEmailOnSubmitEmailEpic';
 import scrollToTopOnViewChangeEpic from './epics/scrollToTopOnViewChangeEpic';
 import submitPaidSubscriptionOnSubmitPaymentEpic from './epics/submitPaidSubscriptionOnSubmitPaymentEpic';
+import submitChargeOnSubmitPaymentEpic from './epics/submitChargeOnSubmitPaymentEpic';
 import redirectToPageOnActionsEpic from './epics/redirectToPageOnActionsEpic';
 import updateStateOnRouteChange from './epics/updateStateOnRouteChange';
 import analyticsEpic from './epics/analyticsEpic';
@@ -11,7 +12,7 @@ export default combineEpics(
   analyticsEpic,
   redirectToPageOnActionsEpic,
   sendEmailOnSubmitEmailEpic,
-  submitPaidSubscriptionOnSubmitPaymentEpic,
+  submitChargeOnSubmitPaymentEpic,
   scrollToTopOnViewChangeEpic,
   updateStateOnRouteChange
 );
