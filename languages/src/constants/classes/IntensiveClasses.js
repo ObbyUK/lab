4545b441e13ec1da5fa5d8e38c8f0942769d751b
max@obby.co.uk <max@obby.co>
2019-01-07 09:41:46 +0000

    Location and date updates

--- languages/src/constants/classes/IntensiveClasses.js
+++ languages/src/constants/classes/IntensiveClasses.js
@@ -4,8 +4,8 @@ export default [
   {
     name: regionNames.CENTRAL_LONDON,
     value: regions.CENTRAL_LONDON,
-    address: "Oxford Circus, W1S 1YR",
-    mapsLink: "https://goo.gl/maps/cmSBWg6z7Xu",
+    address: "Oxford Circus",
+    mapsLink: "https://www.google.com/maps/place/15A+Hanover+St,+Mayfair,+London+W1S+1YH/@51.513724,-0.1443215,17z/data=!3m1!4b1!4m5!3m4!1s0x4876052abfb56037:0x4f9b047a80ba0306!8m2!3d51.513724!4d-0.1421328",
     dates: [
       {
         starts: "17/02/2019",
@@ -54,58 +54,8 @@ export default [
   {
     name: regionNames.CENTRAL_LONDON,
     value: regions.CENTRAL_LONDON,
-    address: "Covent Garden, WC2E 7LL",
-    mapsLink: "https://goo.gl/maps/z2Yq1qL5hdr",
-    dates: [
-      {
-        starts: "17/02/2019",
-        ends: "24/02/2019",
-        lessonsStart: "10:00PM",
-        lessonsEnd: "5:00PM",
-      },
-      {
-        starts: "17/03/2019",
-        ends: "24/03/2019",
-        lessonsStart: "10:00PM",
-        lessonsEnd: "5:00PM",
-      },
-      {
-        starts: "07/04/2019",
-        ends: "14/04/2019",
-        lessonsStart: "10:00PM",
-        lessonsEnd: "5:00PM",
-      },
-      {
-        starts: "12/05/2019",
-        ends: "19/05/2019",
-        lessonsStart: "10:00PM",
-        lessonsEnd: "5:00PM",
-      },
-      {
-        starts: "09/06/2019",
-        ends: "16/06/2019",
-        lessonsStart: "10:00PM",
-        lessonsEnd: "5:00PM",
-      },
-      {
-        starts: "14/07/2019",
-        ends: "21/07/2019",
-        lessonsStart: "10:00PM",
-        lessonsEnd: "5:00PM",
-      },
-      {
-        starts: "11/08/2019",
-        ends: "18/08/2019",
-        lessonsStart: "10:00PM",
-        lessonsEnd: "5:00PM",
-      },
-    ]
-  },
-    {
-    name: regionNames.CENTRAL_LONDON,
-    value: regions.CENTRAL_LONDON,
-    address: "Holborn, WC1V 7QH",
-    mapsLink: "https://goo.gl/maps/ELfyCCte2vu",
+    address: "Chancery Lane",
+    mapsLink: "https://www.google.com/maps/place/6+Hatton+Garden,+London+EC1N+8AA/data=!4m2!3m1!1s0x48761b4da4a11ddf:0x95fcf7fc7a5e1397?ved=2ahUKEwiN4NKOrtvfAhWcRxUIHVYYASUQ8gEwAHoECAAQAQ",
     dates: [
       {
         starts: "17/02/2019",
@@ -154,8 +104,8 @@ export default [
   {
     name: regionNames.WEST_LONDON,
     value: regions.WEST_LONDON,
-    address: "White City, W12 7FP",
-    mapsLink: "https://goo.gl/maps/izBbTMMg7F92",
+    address: "Holland Park",
+    mapsLink: "https://www.google.com/maps/place/Safestay+Holland+Park/@51.5026587,-0.2014231,15z/data=!4m2!3m1!1s0x0:0x33075a6efce7a342?ved=2ahUKEwjkr6ClrtvfAhW6RhUIHdVvBpwQ_BIwCnoECAMQCA",
     dates: [
       {
         starts: "17/02/2019",
@@ -204,8 +154,8 @@ export default [
   {
     name: regionNames.NORTH_LONDON,
     value: regions.NORTH_LONDON,
-    address: "Kings Cross, N1 9SQ",
-    mapsLink: "https://goo.gl/maps/awAeAPJf6rS2",
+    address: "Kings Cross",
+    mapsLink: "https://www.google.com/maps/place/15+Crinan+St,+Kings+Cross,+London+N1+9SQ/data=!4m2!3m1!1s0x48761b3e430485ff:0x12bee3948ae8a429?ved=2ahUKEwjf1oTGrtvfAhWluXEKHSh7C4sQ8gEwAHoECAAQAQ",
     dates: [
       {
         starts: "17/02/2019",
@@ -254,8 +204,8 @@ export default [
   {
     name: regionNames.EAST_LONDON,
     value: regions.EAST_LONDON,
-    address: "Shoreditch, EC2A 4HT",
-    mapsLink: "https://goo.gl/maps/iRgKSeqhECr",
+    address: "Shoreditch",
+    mapsLink: "https://www.google.com/maps/place/100+Clifton+St,+London/@51.5233988,-0.0849796,17z/data=!3m1!4b1!4m5!3m4!1s0x48761cafd04e664d:0x26ed1964c0779193!8m2!3d51.5233955!4d-0.0827909",
     dates: [
       {
         starts: "17/02/2019",
@@ -304,8 +254,8 @@ export default [
   {
     name: regionNames.SOUTH_LONDON,
     value: regions.SOUTH_LONDON,
-    address: "Brixton, SW9 8BQ",
-    mapsLink: "https://goo.gl/maps/PS5xLzzHVEw",
+    address: "Clapham",
+    mapsLink: "https://www.google.com/maps/place/CLapham+North+Art+Centre,+Voltaire+Rd,+London+SW4+6DH/data=!4m2!3m1!1s0x487604510d18f589:0xe6f7cba4d5be9663?ved=2ahUKEwjMzcHVrtvfAhW2ZhUIHZUOAx8Q8gEwAHoECAAQAQ",
     dates: [
       {
         starts: "17/02/2019",
